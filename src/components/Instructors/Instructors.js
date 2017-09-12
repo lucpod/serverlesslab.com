f19b92567a41cac0391a9e5a59356a1583cee029
Luciano Mammino <lucianomammino@gmail.com>
2017-09-12 08:18:41 +0100

    restructuring instructors

--- src/components/Instructors/Instructors.js
+++ src/components/Instructors/Instructors.js
@@ -1,27 +1,51 @@
 import { h } from 'preact'
+import Separator from '../Separator'
 import Instructor from './Instructor'
 import instructors from './data'
+import scientistIcon from '../icons/scientistavatar.svg'
 
 const Instructors = () => (
-  <section id='instructors' className='section instructors'>
-    <div className='container'>
-      <div className='content'>
-        <h2>Our Instructors</h2>
-        <p>Our instructors have proven serverless experience in serverless
-          &nbsp;building complex enterprise applications and delivering workshops
-          &nbsp;and talks in some of the most famous conferences around the globe.
-        </p>
-      </div>
-    </div>
-    <div className='columns is-centered'>
-      { instructors.map((instructor, i) => (
-        <div className='column is-one-quarter'>
-          <Instructor {...instructor} />
+  <div>
+    <Separator downClass='inverted' />
+    <section id='instructors' className='section instructors bg-inverted'>
+      <div className='container'>
+        <div className='columns'>
+          <div className='column'>
+        &nbsp;
+          </div>
+          <div className='column is-three-quarters'>
+            <div className='content'>
+              <h1 className='title'>Our Instructors</h1>
+            </div>
+          </div>
         </div>
-      )) }
-    </div>
 
-  </section>
+        <div className='columns'>
+          <div className='column is-hidden-mobile'>
+            <p className='has-text-centered'>
+              <img className='section-icon' src={scientistIcon} />
+            </p>
+          </div>
+          <div className='column is-three-quarters'>
+            <div className='content'>
+              <p className='is-size-5'>Our instructors have proven serverless experience in serverless
+              &nbsp;building complex enterprise applications and delivering workshops
+              &nbsp;and talks in some of the most famous conferences around the globe.
+              </p>
+            </div>
+            <div className='columns is-centered'>
+              { instructors.map((instructor, i) => (
+                <div className='column is-one-quarter'>
+                  <Instructor {...instructor} />
+                </div>
+              )) }
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <Separator upClass='inverted' />
+  </div>
 )
 
 export default Instructors
